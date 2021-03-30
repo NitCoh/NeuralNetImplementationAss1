9f9b4d4255b65ccbe872be8d7d568430726d6318
krichj <skricheli2@gmail.com>
2021-03-30 18:17:45 +0300

    added mean loss graph

--- backward.py
+++ backward.py
@@ -1,11 +1,9 @@
-from forward import relu, softmax
-
 import numpy as np
 
 
 def linear_backward(dZ, cache):
     """
-    Implements the linear part of the backward propagation process for a single layer
+    The linear part of the backward propagation process for a single layer
     :param dZ: the gradient of the cost with respect to the linear output of the current layer (layer l)
     :param cache: tuple of values (A_prev, W, b) coming from the forward propagation in the current layer
     :return:
@@ -15,11 +13,11 @@ def linear_backward(dZ, cache):
     """
     A_prev = cache["A"]
     W = cache["W"]
-    # linear output = W(A_prev) + b
     m = A_prev.shape[-1]
-    dW = dZ @ A_prev.T
+    dW = dZ @ A_prev.T  # linear output = W(A_prev) + b
     db = np.sum(dZ, axis=1) / m
     dA_prev = W.T @ dZ  # (dL/dZ) @ (dZ/dA_prev)
+
     return dA_prev, dW, db
 
 
@@ -29,7 +27,7 @@ def linear_activation_backward(dA, cache, activation):
      The function first computes dZ and then applies the linear_backward function.
     :param dA: post activation gradient of the current layer
     :param cache: contains both the linear cache and the activations cache
-    :param activation:
+    :param activation: a flag indicating the activation function to use
     :return:
     (dA1) dA_prev – Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
     (dW2) dW – Gradient of the cost with respect to W (current layer l), same shape as W
@@ -46,9 +44,9 @@ def linear_activation_backward(dA, cache, activation):
     dW = dL/dW2
     db = dL/db
     
-    now lets use our functions.
-    we know that calling the activation backwards with dL/dA2 return dZ2
-    to calculate dW2, db2, we need dZ2, so we call linear_backwards which do to the following calc:
+    Now lets use our functions.
+    We know that calling the activation backwards with dL/dA2 return dZ2.
+    To calculate dW2 and db2 we need dZ2, so we call linear_backwards which does the following calculation:
     dW2 = dZ2 @ A1.T
     db2 = dZ2
     dA_prev = dL/dA1 = (dL/dA2=dA in our function) *
@@ -56,14 +54,13 @@ def linear_activation_backward(dA, cache, activation):
                          (dZ2/dA1 = W2)
      = (dL/dZ2) * (dZ2/dA1)
      
-     Each backward_func suppose to get dL/dA_cur as input
+     Each backward_func is supposed to get dL/dA_cur as an input
      and chain by multiplication:
      (dL/dA_cur) @ (dA_cur/dZ_cur)
      
      Assume y_hat = softmax(Z_last)
      The last layer has:
      dA_last=dy_hat=(dL/dy_hat) ==== - (1/m) * [sum[1 to m] of (1/y_hat_i)]
-
     """
 
     def get_derivative(func_act):
@@ -72,8 +69,7 @@ def linear_activation_backward(dA, cache, activation):
         elif func_act == 'softmax':
             return softmax_backward
 
-    backward_func = get_derivative(activation)
-    dZ = backward_func(dA, cache)
+    dZ = get_derivative(activation)(dA, cache)
     dA_prev, dW, db = linear_backward(dZ, cache)
 
     return dA_prev, dW, db
@@ -98,7 +94,7 @@ def relu_backward(dA, activation_cache):
     :param dA:  the post-activation gradient
     :param activation_cache: contains Z (stored during the forward propagation)
     :return:
-    dZ – gradient of the cost with respect to Z 
+    dZ – gradient of the cost with respect to Z
     """
     Z = activation_cache["Z"]
     f = np.vectorize(lambda x: 1 if x > 0 else 0)
@@ -124,14 +120,12 @@ def L_model_backward(AL, Y, caches):
     m = len(Y)
     reversed_cache = reversed(list(enumerate(caches)))
     dA = (AL - Y) / m  # this is dZ actually.
-    for l, cache in reversed_cache:
-        # if l == len(caches) - 1:
-        #     dA_prev, dW, db = linear_activation_backward(dA, cache, 'softmax')
-        # else:
+
+    for layer, cache in reversed_cache:
         dA_prev, dW, db = linear_activation_backward(dA, cache, 'relu')
-        grads['dA' + f'{str(l)}'] = dA
-        grads['dW' + f'{str(l)}'] = dW
-        grads['db' + f'{str(l)}'] = db
+        grads['dA' + f'{str(layer)}'] = dA
+        grads['dW' + f'{str(layer)}'] = dW
+        grads['db' + f'{str(layer)}'] = db
         dA = dA_prev
 
     return grads
@@ -147,8 +141,8 @@ def Update_parameters(parameters, grads, learning_rate):
     parameters – the updated values of the parameters object provided as input
     """
 
-    for i, (W, b) in enumerate(zip(parameters["weights"], parameters["bias"])):
-        parameters["weights"][i] = W - learning_rate * grads['dW' + f'{str(i)}']
-        parameters["bias"][i] = b - learning_rate * np.expand_dims(grads['db' + f'{str(i)}'], axis=-1)
+    for i, (W_i, b_i) in enumerate(zip(parameters["weights"], parameters["bias"])):
+        parameters["weights"][i] = W_i - learning_rate * grads['dW' + f'{str(i)}']
+        parameters["bias"][i] = b_i - learning_rate * np.expand_dims(grads['db' + f'{str(i)}'], axis=-1)
 
     return parameters
--- neural_net.py
+++ neural_net.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from forward import *
 from backward import *
 
@@ -27,22 +29,36 @@ def L_layer_model(X, Y, layers_dims, learning_rate, num_iterations, batch_size):
         length_data = input_data.shape[-1]
         indices = np.array(list(range(length_data)))
         np.random.shuffle(indices)
+
         for i in range(0, length_data, batch_size):
             batch_indices = indices[i:i + batch_size]
-            yield input_data[:,batch_indices], input_labels[:,batch_indices]
+            yield input_data[:, batch_indices], input_labels[:, batch_indices]
 
     cost = -1
     parameters = initialize_parameters(layers_dims)  # dict
+    mean_loses = []
+
     for i in range(num_iterations):
         batch_gen = create_batches(X, Y, batch_size)
+        curr_iteration_mean_loss = 0
+
         for batch_num, (batch, y) in enumerate(batch_gen):
             AL, caches = L_model_forward(batch, parameters, False)
             cost = compute_cost(AL, y)
             grads = L_model_backward(AL, y, caches)
             parameters = Update_parameters(parameters, grads, learning_rate)
             print(f'Iteration {i}, Batch_Num: {batch_num}, Loss: {cost}')
+            curr_iteration_mean_loss += cost / batch_size
 
-        print("=" * 80)
+        mean_loses.append(curr_iteration_mean_loss)
+        print(f"Iteration mean loss: {curr_iteration_mean_loss}\n" + "=" * 80)
+
+
+    plt.plot(list(range(num_iterations)), mean_loses)
+    plt.ylabel('Mean Loss')
+    plt.xlabel('Iteration #')
+    plt.grid()
+    plt.show()
 
     return parameters, cost
 
@@ -51,7 +67,8 @@ def Predict(X, Y, parameters):
     """
     The function receives an input data and the true labels
     and calculates the accuracy of the trained neural network on the data.
-    :param X:  the input data, a numpy array of shape (height*width, number_of_examples)
+
+    :param X: the input data, a numpy array of shape (height*width, number_of_examples)
     :param Y: the “real” labels of the data, a vector of shape (num_of_classes, number of examples)
     :param parameters: a python dictionary containing the DNN architecture’s parameters
     :return:
@@ -59,7 +76,9 @@ def Predict(X, Y, parameters):
     (i.e. the percentage of the samples for which the correct label receives the hughest confidence score).
     Use the softmax function to normalize the output values
     """
+
     y_hat, _ = L_model_forward(X, parameters, False)  # y_hat shape: (num_classes, num_examples)
     y_hat_preds = np.argmax(y_hat, axis=1)
     y_preds = np.argmax(Y, axis=1)
+
     return (y_hat_preds == y_preds).mean()
